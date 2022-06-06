0d814b8bfc3403597ff29fb3c553a360415bba4a
Lorreina Guyett <slorreina369@gmail.com>
2022-06-06 11:38:38 -0700

    pulling user repos

---
+++ assets/js/homepage.js
@@ -0,0 +1,20 @@
+var getUserRepos = function(user){
+    // format the github api url
+    var apiUrl="https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+            console.log(data);
+        });
+    });
+};
+
+getUserRepos();
+
+var response = fetch("https://api.github.com/users/octocat/repos").then(function(response){
+    response.json().then(function(data){
+        console.log(data);
+    });
+});
+console.log(response);
